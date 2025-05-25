cdb08de51c4862c002474bf88eb046659cc41fca
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-05-25 20:40:30 +0200

    terminada

--- landingPixar/src/components/BateBerde.jsx
+++ landingPixar/src/components/BateBerde.jsx
@@ -7,10 +7,11 @@ const BateVerde = () => {
                 width:"68%",
                 height: "270px",
               backgroundImage: `url("https://images.squarespace-cdn.com/content/v1/60241cb68df65b530cd84d95/9ae6d321-34af-4ef8-8b50-525eac838060/mtp_bg.jpg?format=2500w")`, // Verde Pixar
-              padding: "60px 20px",
+           
               display: "flex",
               flexDirection: "row",
                 justifyContent: "space-between",
+                alignItems: "center",
               gap: "10px",
               
             }}
@@ -28,11 +29,16 @@ const BateVerde = () => {
               }}
             />
       
-            <div style={{ width: "400px", color: "#000" }}>
-              <h2 style={{ fontSize: "2rem", marginBottom: "20px" }}>
+              <img src="https://images.squarespace-cdn.com/content/v1/60241cb68df65b530cd84d95/edac5088-045b-4b01-a155-7dc80f975541/MTP_Logo_TT_LtBg_v2.1.png?format=750w" alt="meet" width={350} height={250} style={{marginLeft: "120px"}}/>
+
+
+            <div style={{ width: "400px", color: "#000", display: "flex", flexDirection:"column", justifyContent: "center", alignItems: "center", gap:"10px"}}>
+
+              <img src="https://images.squarespace-cdn.com/content/v1/60241cb68df65b530cd84d95/38214db3-1bde-48b2-9109-99bd864da366/wol-m.png?format=500w" alt="winlose" width={250}/>
+              <h2 style={{ fontSize: "2rem", marginBottom: "20px", color: "white"}}>
                 NOW STREAMING
               </h2>
-              
+              <img src="https://images.squarespace-cdn.com/content/v1/60241cb68df65b530cd84d95/9c284b28-c3bc-4c6f-803a-413ad6520376/d%2B.png?format=500w" alt="logo" width={100} />
               
             </div>
           </section>
--- landingPixar/src/components/WinLose.jsx
+++ landingPixar/src/components/WinLose.jsx
@@ -45,7 +45,7 @@ const WinLose = () => {
             <img style={stylesImg} src="https://images.squarespace-cdn.com/content/v1/60241cb68df65b530cd84d95/471447c0-56ae-46f2-ba47-eb8f93af3c00/wol_logo.png?format=2500w" alt="WinorLose" />
             <h3 style={stylesH3}>ALL EPISODES NOW STREAMING</h3>
             <img style={styleLogo} src="https://images.squarespace-cdn.com/content/v1/60241cb68df65b530cd84d95/1e6a13c7-e61e-4393-9d94-d6d76805037e/d%2B.png?format=750w" alt="LOGO" />
-            <img src="" alt="" />
+           
            
         </div>
      );
