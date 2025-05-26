219c1cbedf15d9b5ed6d6bb28b898c72f4206151
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-05-26 14:19:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/LandingPixar/',
-  plugins: [react()],
+  base: '/LandingPixar/', // ⚠️ Muy importante: el nombre del repo con /
+  plugins: [react()]
 })
