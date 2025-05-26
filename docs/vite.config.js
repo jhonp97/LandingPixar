d048a4b7806ebf9188b400e0a1bcd06b0adaed4f
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-05-26 12:56:08 +0200

    act

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,14 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  // agregar alias @ para la carpeta src
   resolve: {
     alias: {
       "@": "/src"
     }
-  }
+  },
+  base: '/LandingPixar/' // Usa exactamente el nombre del repositorio
 })
